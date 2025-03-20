e47e7044ed3570f71a50e9bbbdf5c6df249a3e6a
Jared Mauch <jared@puck.nether.net>
2025-03-20 14:07:48 -0400

    clarify which things might change inverter settings

--- src/coordinator/commands/mod.rs
+++ src/coordinator/commands/mod.rs
@@ -1,19 +1,19 @@
-pub mod ac_charge_time;
-pub mod ac_first_time;
-pub mod charge_priority_time;
-pub mod forced_discharge_time;
+pub mod parse_hold;
+pub mod parse_input;
 pub mod read_hold;
 pub mod read_inputs;
 pub mod read_param;
+pub mod set_ac_charge_time;
+pub mod set_ac_first_time;
+pub mod set_charge_priority_time;
+pub mod set_forced_discharge_time;
 pub mod set_hold;
 pub mod time_register_ops;
 pub mod timesync;
 pub mod update_hold;
+pub mod validation;
 pub mod write_inverter;
 pub mod write_param;
-pub mod validation;
-pub mod parse_hold;
-pub mod parse_input;
 
 // Re-export common validation functions
 pub use validation::*;
